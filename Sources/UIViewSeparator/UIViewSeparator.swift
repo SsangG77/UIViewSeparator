97cba399d6b843829c69fb2f85b17e96a1a27793
sangjin <sangjincha719@gmail.com>
2025-03-14 18:48:20 +0900

    Updated can use in stackview, scrollview

--- Sources/UIViewSeparator/UIViewSeparator.swift
+++ Sources/UIViewSeparator/UIViewSeparator.swift
@@ -2,20 +2,31 @@
 // https://docs.swift.org/swift-book
 
 
+
+
 import UIKit
 
-public extension UIView {
+extension UIView {
     func addSeparator(color: UIColor = .black, height: CGFloat = 1) {
-        let line = UIView()
-        line.translatesAutoresizingMaskIntoConstraints = false
-        line.backgroundColor = color
-        self.addSubview(line)
+        let separator = UIView()
+        separator.backgroundColor = color
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(separator)
 
-        NSLayoutConstraint.activate([
-            line.heightAnchor.constraint(equalToConstant: height),
-            line.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            line.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            line.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        ])
+        if let stackView = self as? UIStackView {
+            // UIStackView의 경우 arrangedSubview로 추가
+            stackView.addArrangedSubview(separator)
+            NSLayoutConstraint.activate([
+                separator.heightAnchor.constraint(equalToConstant: height)
+            ])
+        } else {
+            // 일반 UIView 및 UIScrollView의 경우 기본적으로 하단에 추가
+            NSLayoutConstraint.activate([
+                separator.heightAnchor.constraint(equalToConstant: height),
+                separator.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                separator.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                separator.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            ])
+        }
     }
 }
